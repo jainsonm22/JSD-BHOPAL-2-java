902f0712903fe0194cc2240091c69f4ae12f4b24
jainsonm22 <jainsonm22@gmail.com>
2024-02-08 19:10:06 +0530

    Account class code

---
+++ ConsDemo.java
@@ -0,0 +1,57 @@
+public class ConsDemo{
+
+String name;
+int age;
+
+public ConsDemo(){
+System.out.println("from defeault constructor");
+}
+
+
+public ConsDemo(String name1 ,int age1)
+{
+name=name1;
+age=age1;
+System.out.println("name of student="+name1);
+System.out.println("name of age="+age1+"\n");
+}
+
+public ConsDemo(int  age1 ,String name1)
+{
+name=name1;
+age=age1;
+System.out.println("name of student="+name1);
+System.out.println("name of age="+age1+"\n");
+}
+
+public ConsDemo(String name1 ,int age1,String address)
+{
+name=name1;
+age=age1;
+System.out.println("name of student="+name1);
+System.out.println("age of user="+age1);
+System.out.println("address of user="+address+"\n");
+
+}
+public ConsDemo(int year,int age1)
+{
+
+age=age1;
+System.out.println("Year of student="+year);
+System.out.println("name of age="+age1+"\n");
+}
+
+public static void main(String[] args){
+
+  ConsDemo obj1= new      ConsDemo("user1",30 ,"indore");
+
+  ConsDemo obj2= new ConsDemo("user1",30);
+
+  ConsDemo obj4= new ConsDemo(30, "user1");
+
+  ConsDemo obj5= new ConsDemo(2024, 30);
+
+  ConsDemo obj3= new ConsDemo();
+
+}
+}
---
+++ HelloClass1.java
@@ -0,0 +1,52 @@
+public class HelloClass1{
+ public static void main(String args[]){
+   System.out.println("hello learners");
+   System.out.println("Good morning learners");
+
+int a=20;
+int b=10;
+
+int result=a%b;
+
+
+System.out.println(++a);
+System.out.println(--b);//b=9
+
+System.out.println("Post increment="+ a++);
+System.out.println(a);
+
+System.out.println("Post decrement="+ b--);//b=8
+System.out.println(b);//b==8
+
+
+int firstVar=10;
+
+firstVar=firstVar+2;//firstVar=12
+System.out.println("firstVar="+firstVar);
+
+firstVar%=2;//firstVar=14
+System.out.println("firstVar after assigment operator ="+firstVar);
+
+int c=30;
+a=20;
+b=10;
+
+System.out.println( "output of and operator ");
+System.out.println( a>b && a==c);
+
+System.out.println( "output of or operator ");
+System.out.println(a>b || a==c);
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}
